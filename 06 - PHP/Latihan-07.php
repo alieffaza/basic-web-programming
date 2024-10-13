34988b52d807086867a25660f1c9a6dd1606eaa2
alieffaza <alieffaza225@gmail.com>
2024-10-13 19:19:39 +0800

    upload new files

---
+++ 06 - PHP/Latihan-07.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello World 1! </br>"; // Perintah ini akan mencetak "Hello World 1!"
+?>
+
+<?php
+/*
+Berikut ini adalah perintah untuk menampilkan 
+teks "Hello World 2!" pada browser
+*/
+echo "Hello World 2! </br>"; 
+?>
+
+<?php
+// Berikut ini adalah perintah
+// untuk menampilkan teks "Hello World 3!"
+// pada browser
+echo "Hello World 3! </br>"; 
+?>
